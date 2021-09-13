3b090e12bb996cad2da9938c8a2b7955e6a4bf96
Felicindo <f.genarito0710@gmail.com>
2021-09-13 14:02:20 -0400

    agregue el archivo ajax_buscar_persona1.php

---
+++ privada/usuarios/ajax_buscar_persona1.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require_once("../../smarty/libs/Smarty.class.php");
+require_once("../../conexion.php");
+require_once("../../resaltarBusqueda.inc.php");
+
+$id_persona = $_POST["id_persona"];
+
+//$db->debug=true;
+$sql3 = $db->Prepare("SELECT *
+                      FROM _personas
+                      WHERE id_persona = ?
+                      AND estado <> 'X'
+");
+$rs3 = $db->GetAll($sql3, array($id_persona));
+
+echo"<center>
+    <table width='60%' border='1'>
+        <tr>
+            <th colspan='4'>Datos Persona</th>
+        </tr>
+";
+foreach ($rs3 as $k => $fila){
+    echo"<tr>
+            <td align='center'>".$fila["ci"]."</td>
+            <td>".$fila["ap"]."</td>
+            <td>".$fila["am"]."</td>
+            <td>".$fila["nombres"]."</td>
+    </tr>";
+}
+echo "</table>
+      </center>";
+
+?>
